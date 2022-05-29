af6e7fb4f829644d3302d00a8df84cc5f8d01cbd
clarettetd <clarette.terrasi@gmail.com>
2022-05-29 17:30:22 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,76 +6,108 @@
 /*   By: cterrasi <cterrasi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 21:21:24 by cterrasi          #+#    #+#             */
-/*   Updated: 2022/05/21 21:41:21 by cterrasi         ###   ########.fr       */
+/*   Updated: 2022/05/29 17:30:05 by cterrasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* Funciona con BUFFER_SIZE bajo... si es un valor muy grande, explota y no se por que.*/
 #include "get_next_line.h"
 
-static char	*extract_line(char **collector, int max_len)
+/*	Esta función se encarga de extraer la línea (desde el inicio hasta el primer \n)
+	del *collector. Pasos:
+	1. Creo una variable char *str, que me va a servir para determinar
+	cuantas posiciones hay hasta el \n.
+	2. Igualo el *str, al *collector.
+	3. Itero sobre *str mientras sea diferente a \n. De este modo, aumento
+	a *str y luego podré hacer resta de punteros para determinar lo descrito
+	en 1.
+	4. Luego de iterar, hago un ft_substr:
+		- De el *collector,
+		- empezando por la posición 0,
+		- voy a extraer un máximo de (str - *collector) <- Esto es una resta de punteros.
+	5. Avanzo al *collector hasta el salto de línea.
+*/
+static void	extract_line(char **collector, char **line)
 {
-	char	*line;
+	char	*str;
 
-	line = ft_substr(*collector, 0, max_len);
-	*collector += max_len;
-	return (line);
+	str = *collector;
+	while (*str != '\n')
+		str++;
+	*line = ft_substr(*collector, 0, str - *collector);
+	*collector += (str - *collector);
 }
 
-/*	Se encarga de leer y componer la línea.
-	Recibe el collector, y lo modifica.
-	El collector, puede venir vacío (estado inicial) o con el resto de la línea.
-*/
-static int	read_and_compose_line(int fd, char **collector)
+/*	Esta función se encarga de detectar si tengo \n dentro de un str (el collector).
+	Es una adaptación del ft_strchr. Pasos:
+	1. Verifico que llegue un str válido; si lo es, continúo, si no, retorno NULL.
+	2. Itero sobre el str:
+		- Si un caracter de str es igual a \n, retorno el str en la 
+		última posicion (CREO).
+		- Si no consigo a \n, retorno NULL. */
+static char	*is_newline(char *str)
 {
-	char	buffer[BUFFER_SIZE];
-	char	*line;
-	int		bytes_read;
+	if (!str)
+		return (NULL);
+	while (*str)
+	{
+		if (*str == '\n')
+		{
+			// printf("CREO: %lu\n", *str + ft_strlen(str));
+			// printf("CREO222: %i\n", *str);
+			return (str + ft_strlen(str));
+		}
+			// Este return (str + ft_strlen(str));
+			// o este return (str); ...DUDOSO...
+		str++;
+	}
+	/*	Si devuelve NULL, significa que no ha encontrado un \n. */
+	return (NULL);
+}
 
-	/*	Leo el fd y retorno los bytes leídos.
-		El collector no ha sido modificado.
-	*/
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read < 0)
-		return (0);
-	/*	Reservo memoria para la línea, con una longitud inicial
-	de los bytes leídos (bytes_read) + 1 */
-	line = malloc(sizeof(char) * (bytes_read + 1));
-	if (!line)
-		return (0);
-	/*	Concateno lo que he leido (buffer), con lo que tenga en la línea;
-	puede que tenga algo o que no tenga nada, porque ella tendrá lo que
-	tenga en el collector en el momento... */
-	ft_strlcat(line, buffer, bytes_read + ft_strlen(line) + 1);
-	*collector = line;
-	free(line);
-	// free(*collector);
-	line = NULL;
-	return (bytes_read);
+/*	Esta función se encarga de leer y componer la línea, transformando a
+	el *collector. Pasos:
+	1. Creo dos variables:
+		- int	bytes_read, para almacenar la cantidad de bytes leídos.
+		- char	*buffer, para almacenar el contenido de lo que he leído.
+	2. Reservo memoria para el buffer, con un tamaño de lo definido en
+	BUFFER_SIZE.
+	3. Si la reserva sale bien, realizo el procedimiento:
+		- Itero mientras lo que consiga en *collector no sea un \n;
+		creo (!is_newline(*collector)).
+		- Dentro del bucle, leo lo que tengo en el fd, lo guardo en el
+		buffer y devuelvo los bytes leidos.
+		- Si la lectura sale mal (bytes_read < 0), entonces libero el buffer.
+		- Si la lectura sale bien, agrego un \0 en la última posición del buffer, y
+		- concateno dentro del collector, lo que tenga en él, más lo que tengo
+		en el buffer.
+		- Por último libero al buffer.
+*/
+static void	read_and_compose_line(int fd, char **collector)
+{
+	int		bytes_read;
+	char	*buffer;
+
+	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (buffer)
+	{
+		while (!is_newline(*collector))
+		{
+			bytes_read = read(fd, buffer, BUFFER_SIZE);
+			if (bytes_read < 0)
+				free(buffer);
+			*(buffer + bytes_read) = '\0';
+			*collector = ft_strjoin(*collector, buffer);
+		}
+		free(buffer);
+	}
 }
 
 char	*get_next_line(int fd)
 {
-	int			i;
 	static char	*collector;
+	char		*line;
 
-	i = 0;
-	/*	Modifico el collector con la primera lectura.*/
 	read_and_compose_line(fd, &collector);
-	/*	Solo itero si tengo algo en el collector. */
-	while (collector)
-	{
-		read_and_compose_line(fd, &collector);
-		printf("%s\n", collector);
-		while (collector[i])
-		{
-			if (collector[i] == '\n')
-				return (extract_line(&collector, i));
-			i++;
-		}
-	}
-	return (collector);
+	extract_line(&collector, &line);
+	return (line);
 }
-
-/* 	Creo que el collector está avanzando según lo que lee y no
-	*/
